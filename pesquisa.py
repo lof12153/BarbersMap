eb6a8decdf933d4fab30b57f66d2d9eb170dfc08
BatistaJnr <joaobatistajunior.029@gmail.com>
2025-11-23 20:56:19 -0300

    Criei a filtragem de busca

--- pesquisa.py
+++ pesquisa.py
@@ -8,6 +8,8 @@ def painel_busca(chave):
     print("|   Barbeiros Disponiveis   |")
     print("=============================")
     cont = 1
+    filtrar_nota = []
+    filtrar_preco = []
     for barbeiro in BARBEIROS:
         media = 0
         preco_medio = 0
@@ -20,18 +22,46 @@ def painel_busca(chave):
         else:
             media = media/len(BARBEIROS[barbeiro]["servicos"])
             preco_medio = preco_medio/len(BARBEIROS[barbeiro]["servicos"])
+        filtrar_nota.append(media)
+        filtrar_preco.append(preco_medio)
         print(f"{cont}° Barbeiro")
         print(f"Nome: {BARBEIROS[barbeiro]["nome"]}")
-        print(f"Nota Media: {media}")
-        print(f"Preço Médio: R${preco_medio}\n")
+        print(f"Nota Media: {media:.2f}")
+        print(f"Preço Médio: R${preco_medio:.2f}\n")
         cont += 1
         if cont >= 10:
             break
     print("=============================")
     print("1 - Pesquisar")
-    print("2 - Filtrar Resultados")
+    print("2 - Filtrar Resultados por Nota")
     print("3 - Sair")
     opc = input("Digite uma opção: ")
+    filtrar_nota.sort(reverse=True)
+    filtrar_preco.sort()
+    for i in range(len(filtrar_preco)):
+        for barbeiro in BARBEIROS:
+            preco_medio = 0
+            for c in range(len(BARBEIROS[barbeiro]["servicos"])):
+                preco_medio += BARBEIROS[barbeiro]["servicos"][c]["valor"]
+            if len(BARBEIROS[barbeiro]["servicos"]) == 0:
+                    preco_medio = 0
+            else:
+                preco_medio = preco_medio/len(BARBEIROS[barbeiro]["servicos"])
+            if filtrar_preco[i] == preco_medio:
+                filtrar_preco[i] = barbeiro
+                break
+    for i in range(len(filtrar_nota)):
+        for barbeiro in BARBEIROS:
+            media = 0
+            for c in range(len(BARBEIROS[barbeiro]["servicos"])):
+                media += sum(BARBEIROS[barbeiro]["servicos"][c]["avaliacao"])/len(BARBEIROS[barbeiro]["servicos"][c]["avaliacao"])
+            if len(BARBEIROS[barbeiro]["servicos"]) == 0:
+                media = 0
+            else:
+                media = media/len(BARBEIROS[barbeiro]["servicos"])
+            if filtrar_nota[i] == media:
+                filtrar_nota[i] = barbeiro
+                break
     while opc != "3":
         if opc == "1":
             nome = input("Digite o nome do barbeiro: ")
@@ -47,14 +77,70 @@ def painel_busca(chave):
                 else:
                     cont = False
             if cont == False:
-                print("Nome não encontrado!")
-                print("1 - Pesquisar")
-                print("2 - Filtrar Resultados")
-                print("3 - Sair")
-                opc = input("Digite uma opção: ")
+                print("Nome não encontrado! Pressione ENTER para continuar.")
+                input()
             break
         elif opc == "2":
-            print("ANDAMENTO")
+            conta = 1
+            os.system("cls")
+            print("=============================")
+            print("|        Barber´sMap        |")
+            print("|   Barbeiros Disponiveis   |")
+            print("=============================")
+            for i in range(len(filtrar_nota)):
+                media = 0
+                preco_medio = 0
+                for I in range(len(BARBEIROS[filtrar_nota[i]]["servicos"])):
+                    media += sum(BARBEIROS[filtrar_nota[i]]["servicos"][I]["avaliacao"])/len(BARBEIROS[filtrar_nota[i]]["servicos"][I]["avaliacao"])
+                    preco_medio += BARBEIROS[filtrar_nota[i]]["servicos"][I]["valor"]
+                if len(BARBEIROS[filtrar_nota[i]]["servicos"]) == 0:
+                    media = 0
+                    preco_medio = 0
+                else:
+                    media = media/len(BARBEIROS[filtrar_nota[i]]["servicos"])
+                    preco_medio = preco_medio/len(BARBEIROS[filtrar_nota[i]]["servicos"])
+                print(f"{conta}° Barbeiro")
+                print(f"Nome: {BARBEIROS[filtrar_nota[i]]["nome"]}")
+                print(f"Nota Media: {media:.2f}")
+                print(f"Preço Médio: R${preco_medio:.2f}\n")
+                conta += 1
+            print("=============================")
+            print("1 - Pesquisar")
+            print("2 - Filtrar Resultados por Preço")
+            print("3 - Sair")
+            opc = input("Digite uma opção: ")
+            os.system("cls")
+            if opc == "2":
+                opc = "FILTRAR PREÇO"
+        elif opc == "FILTRAR PREÇO":
+            conta = 1
+            os.system("cls")
+            print("=============================")
+            print("|        Barber´sMap        |")
+            print("|   Barbeiros Disponiveis   |")
+            print("=============================")
+            for i in range(len(filtrar_preco)):
+                media = 0
+                preco_medio = 0
+                for I in range(len(BARBEIROS[filtrar_preco[i]]["servicos"])):
+                    media += sum(BARBEIROS[filtrar_preco[i]]["servicos"][I]["avaliacao"])/len(BARBEIROS[filtrar_preco[i]]["servicos"][I]["avaliacao"])
+                    preco_medio += BARBEIROS[filtrar_preco[i]]["servicos"][I]["valor"]
+                if len(BARBEIROS[filtrar_preco[i]]["servicos"]) == 0:
+                    media = 0
+                    preco_medio = 0
+                else:
+                    media = media/len(BARBEIROS[filtrar_preco[i]]["servicos"])
+                    preco_medio = preco_medio/len(BARBEIROS[filtrar_preco[i]]["servicos"])
+                print(f"{conta}° Barbeiro")
+                print(f"Nome: {BARBEIROS[filtrar_preco[i]]["nome"]}")
+                print(f"Nota Media: {media:.2f}")
+                print(f"Preço Médio: R${preco_medio:.2f}\n")
+                conta += 1
+            print("=============================")
+            print("1 - Pesquisar")
+            print("2 - Filtrar Resultados por Nota")
+            print("3 - Sair")
+            opc = input("Digite uma opção: ")
         else:
             print("Opção inválida! Digite uma das opções abaixo.")
             print("1 - Pesquisar")
